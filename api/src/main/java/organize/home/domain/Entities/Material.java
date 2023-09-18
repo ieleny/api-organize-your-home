3d110851199f518de7670f5e661cab0c9907750c
Ieleny <ielenyfilgueira@hotmail.com>
2023-09-18 15:26:08 -0300

    feature: Domain entities

---
+++ api/src/main/java/organize/home/domain/Entities/Material.java
@@ -0,0 +1,33 @@
+package organize.home.domain.Entities;
+
+public class Material 
+{
+    private String productName;
+    private Float priceUnd;
+    private Integer quantityBought;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Float getPriceUnd() {
+        return priceUnd;
+    }
+
+    public Integer getQuantityBought() {
+        return quantityBought;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setPriceUnd(Float priceUnd) {
+        this.priceUnd = priceUnd;
+    }
+    
+    public void setQuantityBought(Integer quantityBought) {
+        this.quantityBought = quantityBought;
+    }
+
+}
