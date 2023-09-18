fdb62f291d4c8e07f165b55a50663c00388f6d14
Ieleny <ielenyfilgueira@hotmail.com>
2023-09-18 17:41:52 -0300

    feature: Create JdbcConfiguration

---
+++ api/src/main/java/organize/home/api/infrastructure/persistence/JdbcConfiguration.java
@@ -0,0 +1,16 @@
+package organize.home.api.infrastructure.persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class JdbcConfiguration 
+{
+   @Bean
+   public JdbcTemplate jdbcTemplate(HikariDataSource hikariDataSource){
+       return new JdbcTemplate(hikariDataSource);
+   }
+}
