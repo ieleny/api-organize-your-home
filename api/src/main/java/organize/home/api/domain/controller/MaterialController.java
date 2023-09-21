c21d9d379e7b09a823586fbeac257b5b3485726f
Ieleny <ielenyfilgueira@hotmail.com>
2023-09-21 17:58:48 -0300

    feature: Add path the annotanios

--- api/src/main/java/organize/home/api/domain/controller/MaterialController.java
+++ api/src/main/java/organize/home/api/domain/controller/MaterialController.java
@@ -3,10 +3,12 @@ package organize.home.api.domain.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
