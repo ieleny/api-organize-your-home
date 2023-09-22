4b72ee63dd7852bee0c05314b0353a0d6c2bb526
Ieleny <ielenyfilgueira@hotmail.com>
2023-09-22 11:51:55 -0300

    Test: Apply test for the list

--- api/src/test/java/organize/home/api/ApiApplicationTests.java
+++ api/src/test/java/organize/home/api/ApiApplicationTests.java
@@ -1,13 +1,25 @@
 package organize.home.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import organize.home.api.domain.entities.Material;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
+	@Test
+	void testListOfMaterial() {
+		ResponseEntity<Material[]> response = testRestTemplate.getForEntity("/api/v1/material", Material[].class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
 }
